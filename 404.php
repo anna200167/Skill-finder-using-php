ca0d0865d9d9c0f6beaa48984bd8d7e83d720afa
anna200167 <87202388+anna200167@users.noreply.github.com>
2022-07-21 09:09:57 +0530

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error</title>
+    <style>
+ .hit-the-floor {
+  color: #000;
+  font-size: 12em;
+  font-weight: bold;
+  font-family: Helvetica;
+  text-align: center;
+  padding-top:12px;
+  box-shadow:2px 2px 12px;
+  }
+body {
+  background-color: #f9f9f9;
+}
+    </style>
+</head>
+<body>
+<div class="hit-the-floor">404</div>
+
+</body>
+</html>
